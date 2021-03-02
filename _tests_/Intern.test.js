418e764e376ca2a066a06958982c03a239f813b1
Kalib Hicks <kalib.hicks@gmail.com>
2021-03-02 18:50:03 -0500

    intern test

---
+++ _tests_/Intern.test.js
@@ -0,0 +1,20 @@
+const Intern = require("../lib/Intern");
+
+test("github test", () => {
+    const testValue = "GitHubUser";
+    const e = new Engineer("Foo", 1, "test@test.com", testValue);
+    expect(e.github).toBe(testValue);
+  });
+
+  test("get intern role", () => {
+    const testValue = "Intern";
+    const e = new Engineer("Foo", 1, "test@test.com", "GitHubUser");
+    expect(e.getRole()).toBe(testValue);
+  });
+
+  test("Can get GitHub username via getGithub()", () => {
+    const testValue = "GitHubUser";
+    const e = new Engineer("Foo", 1, "test@test.com", testValue);
+    expect(e.getGithub()).toBe(testValue);
+  });
+  
