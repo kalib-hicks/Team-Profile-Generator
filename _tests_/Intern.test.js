a15893a0aa7cd727a7a331adafd5ee8b6afdfaa3
Kalib Hicks <kalib.hicks@gmail.com>
2021-03-02 18:54:28 -0500

    manager.js

--- _tests_/Intern.test.js
+++ _tests_/Intern.test.js
@@ -2,19 +2,19 @@ const Intern = require("../lib/Intern");
 
 test("github test", () => {
     const testValue = "GitHubUser";
-    const e = new Engineer("Foo", 1, "test@test.com", testValue);
+    const e = new Intern("Foo", 1, "test@test.com", testValue);
     expect(e.github).toBe(testValue);
   });
 
   test("get intern role", () => {
     const testValue = "Intern";
-    const e = new Engineer("Foo", 1, "test@test.com", "GitHubUser");
+    const e = new Intern("Foo", 1, "test@test.com", "GitHubUser");
     expect(e.getRole()).toBe(testValue);
   });
 
   test("Can get GitHub username via getGithub()", () => {
     const testValue = "GitHubUser";
-    const e = new Engineer("Foo", 1, "test@test.com", testValue);
+    const e = new Intern("Foo", 1, "test@test.com", testValue);
     expect(e.getGithub()).toBe(testValue);
   });
   
