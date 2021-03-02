d5e31a0815b12c62c989827692f8a0e5072a1f68
Kalib Hicks <kalib.hicks@gmail.com>
2021-03-02 18:34:17 -0500

    engineer test

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -5,3 +5,16 @@ test("github test", () => {
     const e = new Engineer("Foo", 1, "test@test.com", testValue);
     expect(e.github).toBe(testValue);
   });
+
+  test("get engineer role", () => {
+    const testValue = "Engineer";
+    const e = new Engineer("Foo", 1, "test@test.com", "GitHubUser");
+    expect(e.getRole()).toBe(testValue);
+  });
+
+  test("Can get GitHub username via getGithub()", () => {
+    const testValue = "GitHubUser";
+    const e = new Engineer("Foo", 1, "test@test.com", testValue);
+    expect(e.getGithub()).toBe(testValue);
+  });
+  
