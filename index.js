73e218c8f8ebe92bc48e543e6e37ecf3b2dc76d0
Kalib Hicks <kalib.hicks@gmail.com>
2021-03-02 18:58:20 -0500

    const required

---
+++ index.js
@@ -0,0 +1,6 @@
+const inquirer = require("inquirer");
+const jest = require("jest");
+const Employee = require("./lib/Employee");
+const Engineer = require("./lib/Engineer");
+const Manager = require("./lib/Manager");
+const Intern = require("./lib/Intern");
