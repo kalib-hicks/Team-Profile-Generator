9ddb7cebab10d4fa7d70996cd35dc3a588d6e574
Kalib Hicks <kalib.hicks@gmail.com>
2021-03-02 19:09:13 -0500

    added question prompt and answers

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// const required
 const inquirer = require("inquirer");
 const jest = require("jest");
 const fs = require("fs");
@@ -5,3 +6,39 @@ const Employee = require("./lib/Employee");
 const Engineer = require("./lib/Engineer");
 const Manager = require("./lib/Manager");
 const Intern = require("./lib/Intern");
+
+// employees array
+const employees = [];
+
+//start webpage
+function initIndex() {
+    startHtml();
+    addMember();
+}
+
+function addMember() {
+    inquirer.prompt([{
+        message: "Name"
+        name: "name"
+    },
+    {
+        type: "list",
+        messgae: "Role",
+        choices: [
+            "Intern",
+            "Engineer",
+            "Manager"
+        ],
+        
+        name: "role"
+    },
+    {
+        message: "Id number",
+        name: "id"
+    },
+    {
+        message: "Email",
+        name: "email"
+    }
+])
+}
