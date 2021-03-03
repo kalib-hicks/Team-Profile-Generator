f942e261fbead6a8d1cb1d16547c36ca84892376
Kalib Hicks <kalib.hicks@gmail.com>
2021-03-02 20:44:06 -0500

    copy html file into new folder

--- utils/generate-site.js
+++ utils/generate-site.js
@@ -14,3 +14,21 @@ const writeFile = fileContent => {
         });
     });
   };
+
+  const copyFile = () => {
+    return new Promise((resolve, reject) => {
+      fs.copyFile('./src/style.css', './dist/style.css', err => {
+          if (err) {
+              reject (err);
+              return;
+          }
+
+          resolve({
+              ok: true,
+              message: 'Your Team Roster Has Been Created!'
+          });
+      });
+    });
+  };
+
+module.exports = {writeFile, copyFile};
