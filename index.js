6975575ea5de9a47c017e877490190783409e3ee
Kalib Hicks <kalib.hicks@gmail.com>
2021-03-02 19:39:22 -0500

    writefile

--- index.js
+++ index.js
@@ -100,3 +100,16 @@ function startHtml() {
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <title>Team Profile</title>
     </head>
+    <body>
+    <nav class="navbar navbar-dark bg-dark mb-5">
+        <span class="navbar-brand mb-0 h1 w-100 text-center">Team Profile</span>
+    </nav>
+    <div class="container">
+        <div class="row">`;
+fs.writeFile("./output/team.html", html, function(err) {
+    if (err) {
+        console.log(err);
+    }
+});
+console.log("start");
+}
